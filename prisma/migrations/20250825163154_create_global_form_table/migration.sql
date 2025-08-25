9e57d1ea45493c1fd041dcb1b1f6c2b1d3ac02ea
David Alejandro Mateus Martinez <davidalejandro.mateus.contractor@bbva.com>
2025-08-25 11:33:06 -0500

    feat: Cambiar el proveedor de la base de datos de SQLite a PostgreSQL y agregar migración para crear la tabla 'formInfo'.

--- prisma/migrations/20250825163154_create_global_form_table/migration.sql
+++ prisma/migrations/20250825163154_create_global_form_table/migration.sql
@@ -1,12 +1,14 @@
 -- CreateTable
 CREATE TABLE "formInfo" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name_client" TEXT NOT NULL,
     "name_company" TEXT NOT NULL,
     "email_client" TEXT NOT NULL,
     "sector_client" TEXT NOT NULL,
     "is_active" BOOLEAN NOT NULL DEFAULT true,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME,
-    "deleted_at" DATETIME
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "formInfo_pkey" PRIMARY KEY ("id")
 );
